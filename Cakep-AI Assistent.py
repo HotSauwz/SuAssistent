0de535581e7f424e0e292f61fd2f4cb1f629eb8d
SocniTheForker <93401131+SocniTheForker@users.noreply.github.com>
2021-12-20 09:18:48 +0100

    Version 0.1 of the Cakep-AI Assistent
    Its a unoptimized mess but it works.
    
    I made this all for a single person
    She is a linux beginner so everything has been centered around linux beginners.

---
+++ Cakep-AI Assistent.py
@@ -0,0 +1,329 @@
+#   MADE BY:                                                                  Please dont remove the author thingy :)
+#
+#       _______.  ______     ______ .__   __.  __  
+#     /       | /  __  \   /      ||  \ |  | |  |                             Handy useless assistent tool
+#    |   (----`|  |  |  | |  ,----'|   \|  | |  |                             19-12-2021
+#     \   \    |  |  |  | |  |     |  . `  | |  | 
+# .----)   |   |  `--'  | |  `----.|  |\   | |  | 
+# |_______/     \______/   \______||__| \__| |__| 
+#
+# ___________.__                                                           
+# \__    ___/|  |__   ____  
+#   |    |   |  |  \_/ __ \ 
+#   |    |   |   Y  \  ___/ 
+#   |____|   |___|  /\___  >
+#                 \/     \/ 
+# 
+#  .____              \                 
+#  /       __.  .___  |   ,   ___  .___ 
+#  |__.  .'   \ /   \ |  /  .'   ` /   \
+#  |     |    | |   ' |-<   |----' |   '
+#  /      `._.' /     /  \_ `.___, /    
+#                                      
+# Dependencies: grep, curl, sed, mpv, ffmpeg, ani-cli and ofcourse python
+# All dependencies can be installed from inside the program
+
+
+
+
+
+
+import os
+import time
+
+
+os.system("clear")
+print("  ______     ___       __  ___  _______ .______             ___       __  ")
+print(" /      |   /   \     |  |/  / |   ____||   _  \           /   \     |  | ")
+print("|  ,----'  /  ^  \    |  '  /  |  |__   |  |_)  |  ______ /  ^  \    |  | ")
+print("|  |      /  /_\  \   |    <   |   __|  |   ___/  |______/  /_\  \   |  | ")
+print("|  `----./  _____  \  |  .  \  |  |____ |  |            /  _____  \  |  | ")
+print(" \______/__/     \__\ |__|\__\ |_______|| _|           /__/     \__\ |__| ")
+print("               _     _             _   ")
+print("  __ _ ___ ___(_)___| |_ ___ _ __ | |_ ")
+print(" / _` / __/ __| / __| __/ _ \ '_ \| __|")
+print("| (_| \__ \__ \ \__ \ ||  __/ | | | |_ ")
+print(" \__,_|___/___/_|___/\__\___|_| |_|\__|")
+print("")
+print("By Socni The Forker")
+time.sleep(1)
+
+os.system("clear")
+print("")
+print ("____ ___                                                 ")
+print("|    |   \______ ___________  ____ _____    _____   ____  ")
+print("|    |   /  ___// __ \_  __ \/    \\__  \  /     \_/ __ \ ")
+print("|    |  /\___ \\  ___/|  | \/   |  \/ __ \|  Y Y  \  ___/ ")
+print("|______//____  >\___  >__|  |___|  (____  /__|_|  /\___  >")
+print("             \/     \/           \/     \/      \/     \/ ")
+print("___________       __                 ")
+print("\_   _____/ _____/  |________ ___.__.")
+print(" |    __)_ /    \   __\_  __ <   |  |")
+print(" |        \   |  \  |  |  | \/\___  |")
+print("/_______  /___|  /__|  |__|   / ____|")
+print("        \/     \/             \/     ")
+print("")
+print("Hello and welcome to Cakep-AI Assistent, Please enter your username")
+Username = input()
+os.system("clear")
+
+print("    __  __                   ")
+print("   / / / /___  ____ ___  ___ ")
+print("  / /_/ / __ \/ __ `__ \/ _ \ ")
+print(" / __  / /_/ / / / / / /  __/")
+print("/_/ /_/\____/_/ /_/ /_/\___/ ")
+print("")
+print("")
+print("")
+print("Welcome Back, " + Username)
+print("")
+print("How can i help you today? " + Username)
+print("")
+print("1: Local Weather")
+print("2: Current system specifications")
+print("3: Watch Anime")
+print("4: Matrix Screensaver")
+print("5: Install required programs")
+print("6: Listen to music")
+print("7: Internet Tools")
+print("8: Quit Cakep-AI Assistent")
+
+selection = input()
+if selection == "1":
+    os.system("clear")
+    print("                /    /             ")
+    print("      ___  ___ (___ (___  ___  ___ ")
+    print("|   )|___)|   )|    |   )|___)|   )")
+    print("|/\/ |__  |__/||__  |  / |__  |    ")
+
+    os.system("curl wttr.in")
+    print("Type quit to quit")
+    option = input()
+    if option == "quit":
+            os.system("clear")
+            print("                                                     _   ")
+            print(" ___  ___  ___   _   _  ___  _   _   _ __   _____  _| |_ ")
+            print("/ __|/ _ \/ _ \ | | | |/ _ \| | | | | '_ \ / _ \ \/ / __|")
+            print("\__ \  __/  __/ | |_| | (_) | |_| | | | | |  __/>  <| |_ ")
+            print("|___/\___|\___|  \__, |\___/ \__,_| |_| |_|\___/_/\_\\__|")
+            print("                 |___/                                   ")
+            print(" _   _                    ____  ")
+            print("| |_(_)_ __ ___   ___   _|  _ \ ")
+            print("| __| | '_ ` _ \ / _ \ (_) | | |")
+            print("| |_| | | | | | |  __/  _| |_| |")
+            print(" \__|_|_| |_| |_|\___| (_)____/ ")
+            time.sleep(2)
+            quit
+if selection == "2":
+    os.system("neofetch")
+    print("Type quit to quit")
+    option = input()
+    if option == "quit":
+            os.system("clear")
+            print("                                                     _   ")
+            print(" ___  ___  ___   _   _  ___  _   _   _ __   _____  _| |_ ")
+            print("/ __|/ _ \/ _ \ | | | |/ _ \| | | | | '_ \ / _ \ \/ / __|")
+            print("\__ \  __/  __/ | |_| | (_) | |_| | | | | |  __/>  <| |_ ")
+            print("|___/\___|\___|  \__, |\___/ \__,_| |_| |_|\___/_/\_\\__|")
+            print("                 |___/                                   ")
+            print(" _   _                    ____  ")
+            print("| |_(_)_ __ ___   ___   _|  _ \ ")
+            print("| __| | '_ ` _ \ / _ \ (_) | | |")
+            print("| |_| | | | | | |  __/  _| |_| |")
+            print(" \__|_|_| |_| |_|\___| (_)____/ ")
+            time.sleep(2)
+            quit
+if selection == "3":
+    os.system("clear")
+    print("      ___           ___                       ___           ___     ")
+    print("     /\  \         /\__\          ___        /\__\         /\  \    ")
+    print("    /::\  \       /::|  |        /\  \      /::|  |       /::\  \   ")
+    print("   /:/\:\  \     /:|:|  |        \:\  \    /:|:|  |      /:/\:\  \  ")
+    print("  /::\~\:\  \   /:/|:|  |__      /::\__\  /:/|:|__|__   /::\~\:\  \ ")
+    print(" /:/\:\ \:\__\ /:/ |:| /\__\  __/:/\/__/ /:/ |::::\__\ /:/\:\ \:\__\ ")
+    print(" \/__\:\/:/  / \/__|:|/:/  / /\/:/  /    \/__/~~/:/  / \:\~\:\ \/__/")
+    print("      \::/  /      |:/:/  /  \::/__/           /:/  /   \:\ \:\__\  ")
+    print("      /:/  /       |::/  /    \:\__\          /:/  /     \:\ \/__/  ")
+    print("     /:/  /        /:/  /      \/__/         /:/  /       \:\__\    ")
+    print("     \/__/         \/__/                     \/__/         \/__/ ")
+    print("")
+    print("")
+    print("")
+    print("Hello," + Username ); print("Please choose one of the following options")
+    print("")
+    print("1: Install dependencies (requires sudo)")
+    print("2: Watch Anime")
+    print("3: Quit")
+    ani = input() #anime selection tool
+    if ani == "1":
+        os.system("xdg-open https://github.com/pystardust/ani-cli/blob/master/README.md")
+        os.system("sudo apt install grep curl sed mpv ffmpeg")
+        print("Cakep-AI finished installing the dependencies.")
+    if ani == "2":
+        os.system("clear")
+        print("  ______     ___       __  ___  _______ .______             ___       __  ")
+        print(" /      |   /   \     |  |/  / |   ____||   _  \           /   \     |  | ")
+        print("|  ,----'  /  ^  \    |  '  /  |  |__   |  |_)  |  ______ /  ^  \    |  | ")
+        print("|  |      /  /_\  \   |    <   |   __|  |   ___/  |______/  /_\  \   |  | ")
+        print("|  `----./  _____  \  |  .  \  |  |____ |  |            /  _____  \  |  | ")
+        print(" \______/__/     \__\ |__|\__\ |_______|| _|           /__/     \__\ |__| ")
+        print("")
+        print(":::'###::::'##::: ##:'####:'##::::'##:'########:")
+        print("::'## ##::: ###:: ##:. ##:: ###::'###: ##.....::")
+        print(":'##:. ##:: ####: ##:: ##:: ####'####: ##:::::::")
+        print("'##:::. ##: ## ## ##:: ##:: ## ### ##: ######:::")
+        print(" #########: ##. ####:: ##:: ##. #: ##: ##...::::")
+        print(" ##.... ##: ##:. ###:: ##:: ##:.:: ##: ##:::::::")
+        print(" ##:::: ##: ##::. ##:'####: ##:::: ##: ########:")
+        print("..:::::..::..::::..::....::..:::::..::........::")
+        print("")
+        print("")
+        print("")
+        print("Anime Time! Choose one of the following options")
+        print("")
+        print("1: Search anime")
+        print("2: Save anime to system")
+        print("3: Watch History")
+        print("4: Quit")
+        Cani = input() #cakep-AI ani
+        if Cani == "1":
+            os.system("ani-cli")
+        if Cani == "2":
+            os.system("ani-cli -d")
+        if Cani == "3":
+            os.system("ani-cli -H")
+        if Cani == "4":
+                print("Type quit to quit")
+                option = input()
+                if option == "quit":
+                    os.system("clear")
+                    print("                                                     _   ")
+                    print(" ___  ___  ___   _   _  ___  _   _   _ __   _____  _| |_ ")
+                    print("/ __|/ _ \/ _ \ | | | |/ _ \| | | | | '_ \ / _ \ \/ / __|")
+                    print("\__ \  __/  __/ | |_| | (_) | |_| | | | | |  __/>  <| |_ ")
+                    print("|___/\___|\___|  \__, |\___/ \__,_| |_| |_|\___/_/\_\\__|")
+                    print("                 |___/                                   ")
+                    print(" _   _                    ____  ")
+                    print("| |_(_)_ __ ___   ___   _|  _ \ ")
+                    print("| __| | '_ ` _ \ / _ \ (_) | | |")
+                    print("| |_| | | | | | |  __/  _| |_| |")
+                    print(" \__|_|_| |_| |_|\___| (_)____/ ")
+                    time.sleep(2)
+                    quit
+    if ani == "3:":
+        print("Type quit to quit")
+        option = input()
+        if option == "quit":
+            os.system("clear")
+            print("                                                     _   ")
+            print(" ___  ___  ___   _   _  ___  _   _   _ __   _____  _| |_ ")
+            print("/ __|/ _ \/ _ \ | | | |/ _ \| | | | | '_ \ / _ \ \/ / __|")
+            print("\__ \  __/  __/ | |_| | (_) | |_| | | | | |  __/>  <| |_ ")
+            print("|___/\___|\___|  \__, |\___/ \__,_| |_| |_|\___/_/\_\\__|")
+            print("                 |___/                                   ")
+            print(" _   _                    ____  ")
+            print("| |_(_)_ __ ___   ___   _|  _ \ ")
+            print("| __| | '_ ` _ \ / _ \ (_) | | |")
+            print("| |_| | | | | | |  __/  _| |_| |")
+            print(" \__|_|_| |_| |_|\___| (_)____/ ")
+            time.sleep(2)
+            quit
+if selection == "4":
+    os.system("cmatrix")
+if selection == "5":
+    os.system("sudo apt install grep curl sed mpv ffmpeg cmatrix moc")
+if selection == "6":
+    os.system("mocp")
+
+if selection == "7":
+    os.system("clear")
+    print("      ___           ___           ___     ")
+    print("     /__/\         /__/\         /__/\    ")
+    print("    _\_ \:\       _\_ \:\       _\_ \:\   ")
+    print("   /__/\ \:\     /__/\ \:\     /__/\ \:\  ")
+    print("  _\_ \:\ \:\   _\_ \:\ \:\   _\_ \:\ \:\ ")
+    print(" /__/\ \:\ \:\ /__/\ \:\ \:\ /__/\ \:\ \:\ ")
+    print(" \  \:\ \:\/:/ \  \:\ \:\/:/ \  \:\ \:\/:/")
+    print("  \  \:\ \::/   \  \:\ \::/   \  \:\ \::/ ")
+    print("   \  \:\/:/     \  \:\/:/     \  \:\/:/  ")
+    print("    \  \::/       \  \::/       \  \::/   ")
+    print("     \__\/         \__\/         \__\/    ")
+    print("")
+    print("")
+    print("")
+    print("Hello, " + Username)
+    print("What website would you like to visit")
+    print("")
+    print("1: DuckDuckGo")
+    print("2: YouTube")
+    print("3: Odysee")
+    print("4: Gnome Look")
+    print("5: Linux Wiki")
+    print("6: ProtonDB")
+    print("7: ProtonMail")
+    print("8: Twitch")
+    print("9: Current News")
+    print("0: quit")
+    www = input()
+
+    if www == "1":
+        os.system("xdg-open https://duck.com")
+    
+    if www == "2":
+        os.system("xdg-open https://youtube.com")
+
+    if www == "3":
+        os.system("xdg-open https://odysee.com")
+
+    if www == "4":
+        os.system("xdg-open https://gnome-look.org")
+
+    if www == "5":
+        os.system("xdg-open https://linux.fandom.com/wiki/Linux_Wiki")
+    
+    if www == "6":
+        os.system("xdg-open https://protondb.com")
+    
+    if www == "7":
+        os.system ("xdg-open https://https://account.protonmail.com/login")
+    
+    if www == "8":
+        os.system ("xdg-open https://twitch.tv")
+    
+    if www == "9":
+        os.system ("curl getnews.tech")
+    
+    if www == "0":
+        os.system("clear")
+        print("                                                     _   ")
+        print(" ___  ___  ___   _   _  ___  _   _   _ __   _____  _| |_ ")
+        print("/ __|/ _ \/ _ \ | | | |/ _ \| | | | | '_ \ / _ \ \/ / __|")
+        print("\__ \  __/  __/ | |_| | (_) | |_| | | | | |  __/>  <| |_ ")
+        print("|___/\___|\___|  \__, |\___/ \__,_| |_| |_|\___/_/\_\\__|")
+        print("                 |___/                                   ")
+        print(" _   _                    ____  ")
+        print("| |_(_)_ __ ___   ___   _|  _ \ ")
+        print("| __| | '_ ` _ \ / _ \ (_) | | |")
+        print("| |_| | | | | | |  __/  _| |_| |")
+        print(" \__|_|_| |_| |_|\___| (_)____/ ")
+        time.sleep(2)
+        quit
+
+if selection == "8":
+    os.system("clear")
+    print("                                                     _   ")
+    print(" ___  ___  ___   _   _  ___  _   _   _ __   _____  _| |_ ")
+    print("/ __|/ _ \/ _ \ | | | |/ _ \| | | | | '_ \ / _ \ \/ / __|")
+    print("\__ \  __/  __/ | |_| | (_) | |_| | | | | |  __/>  <| |_ ")
+    print("|___/\___|\___|  \__, |\___/ \__,_| |_| |_|\___/_/\_\\__|")
+    print("                 |___/                                   ")
+    print(" _   _                    ____  ")
+    print("| |_(_)_ __ ___   ___   _|  _ \ ")
+    print("| __| | '_ ` _ \ / _ \ (_) | | |")
+    print("| |_| | | | | | |  __/  _| |_| |")
+    print(" \__|_|_| |_| |_|\___| (_)____/ ")
+    time.sleep(2)
+    quit
+
+
+    
